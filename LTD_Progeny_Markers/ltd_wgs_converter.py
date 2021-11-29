de14361ec1bc4f04cc3db3c40deaeb90efac755d
fillinger.22@osu.edu <fillinger.22@buckeyemail.osu.edu>
2021-11-29 16:23:04 -0500

    Began writing function to convert LTD Progey WGS data to 4way data to run it through the pipeline.

--- LTD_Progeny_Markers/ltd_wgs_converter.py
+++ LTD_Progeny_Markers/ltd_wgs_converter.py
@@ -56,6 +56,93 @@ def parse_path( path_n_name, sep = "/" ):
 	return dir_path, file_name
 
 
+def marker_cleaner(sample_list, file_name): 
+	"""This function removes markers that are missing from eiter parent and markers that are
+	ambiguous between parents (the parents share an allele). """
+
+	keepers = []
+	chuckers = []
+
+	length = len(sample_list[0])
+
+	chromosomes = sample_list[0]
+	chr_pos     = sample_list[1]
+	parent1     = sample_list[2] #SC5314
+	parent2     = sample_list[3] #529L or P60002
+
+	for index in range(1,length): # Skip the header; start at 1
+
+		try: 
+			p1b = parent1[index][0] + parent1[index][-1]
+			p2b = parent2[index][0] + parent2[index][-1]
+		except IndexError: 
+			print( "Something is wrong with the parental genotypes: ", chromosomes[index], ": ", chr_pos[index] )
+			print( "Parent 1 (SC): ", p1b )
+			print( "Parent 2 (529L/P6): ", p2b )
+
+		try:
+			chr_pos[index] = int( chr_pos[index] ) # Convert to ints so they can get sorted properly
+		except IndexError: 
+			print( "Something is wrong with the chromosome position: ", chromosomes[index], ": ", chr_pos[index] )
+			quit()
+
+		# if they share an allele! Make sure I know...
+		chucker_bool = (p1b[0] == p2b[0]) or (p1b[0] == p2b[1]) or (p1b[1] == p2b[0]) or (p1b[1] == p2b[1])
+
+		if chucker_bool:
+			chuckers.append( index )
+		else: 
+			keepers.append( index )
+
+	# Convert the data to 4way to just put it through the pipeline! It'll be much simpler that way.  
+
+
+def four_to_F2( cleaned_file_name ):
+	
+	"""Converts 4-way alele data to F2 
+	ALL INPUT INTO THIS FUNCTION MUST GO THROUGH THE 'marker_cleaner' FUNCTION FIRST!!!"""
+	dir_path, file_name = parse_path( cleaned_file_name )
+	cleaned_marker_file = open( dir_path + file_name, "r" )
+	untrp_cleaned_markers = csv_reader( cleaned_marker_file )
+	cleaned_markers = transpose( untrp_cleaned_markers )
+
+	f2_array = []
+
+	header = 1
+	for lines in cleaned_markers: 
+		
+		if header: 
+			f2_array.append( lines )
+			header = 0
+			continue		
+
+		parent1 = lines[3]
+		parent2 = lines[4]
+
+		new_line = copy.deepcopy(lines[:3]) # Creates the new marker for conversion and adds alleles for parent 1 and 2 (since we know they're already mutually exclusive)
+		new_line.append( "1" )
+		new_line.append( "2" )
+
+		for n, elements in enumerate(lines[5:]): # This DOES NOT check for post-parasex LOH events ("dd" and perhaps "cc" are examples). Not sure how important those are, but they'll be called "n." 
+
+			if "-" in elements: 
+				new_line.append( "-" )
+			elif elements == parent1: 
+				new_line.append( "1" )
+			elif elements == parent2: 
+				new_line.append( "2" )
+			else:
+				new_line.append( "n" )
+
+		f2_array.append( new_line )
+
+	trp_f2 = transpose( f2_array )
+
+	new_file = open( dir_path + "f2_" + file_name, "w")
+	csv_printer( trp_f2, new_file )
+	new_file.close()
+
+
 def main():
 
 	arg_list = []
@@ -83,22 +170,26 @@ def main():
 	het_var       = data_list[8] # Number of samples with 2 alleles that don't belong to SC5314 - both are from the other parent
 	
 	# Genotypes for each parent and progeny
-	SC5314        = data_list[9]
-	five29L       = data_list[10]
-	P60002        = data_list[11]
-	MAY_103       = data_list[12]
-	MAY_316       = data_list[13]
-	MAY_154       = data_list[14]
-	MAY_155       = data_list[15]
-	MAY_156       = data_list[16]
-	MAY_157       = data_list[17]
-	MAY_158       = data_list[18]
-	MAY_332	      = data_list[19]
-	MAY_333       = data_list[20]
+	SC5314                 = data_list[9]
+	five29L                = data_list[10]
+	P60002                 = data_list[11]
+	MAY_103_529L_tet       = data_list[12] # SCx529L Tetraploid
+	MAY_316_P600_tet       = data_list[13] # SCxP6 Tetraploid
+	MAY_154_529L_prog      = data_list[14] # SCx529L Progeny
+	MAY_155_529L_prog      = data_list[15] # SCx529L Progeny
+	MAY_156_529L_prog      = data_list[16] # SCx529L Progeny
+	MAY_157_529L_prog      = data_list[17] # SCx529L Progeny
+	MAY_158_529L_prog      = data_list[18] # SCx529L Progeny
+	MAY_332_P600_prog      = data_list[19] # SCxP60002 Progeny
+	MAY_333_P600_prog      = data_list[20] # SCxP60002 Progeny
 
-	# for lines in data_list: 
-	# 	print( lines[0] )
+	SCx529L = [ chromosome, chr_pos, SC5314, five29L, MAY_103_529L_tet, MAY_154_529L_prog, MAY_155_529L_prog, MAY_156_529L_prog, MAY_157_529L_prog, MAY_158_529L_prog ]
+	SCxP6   = [ chromosome, chr_pos, SC5314, P60002, MAY_316_P600_tet, MAY_332_P600_prog, MAY_333_P600_prog ]
 
-	# Next thing to do is to convert the parental genotypes into 1's, 2's, and n's
+	print( "SC5314 x 529L" )
+	marker_cleaner( SCx529L, "cleaned_SCx529L.csv" )
+	print() 
+	print( "SC5314 x P60002" )
+	marker_cleaner( SCxP6, "cleaned_SCxP6.csv" )
 
 main()
